8064ecb640dd7daab5ce2b3525a9976064df0ca7
pratamadli <adlipratama09@gmail.com>
2024-04-11 23:57:51 +0700

    fixing navbar

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -1,14 +1,16 @@
-import { Meta } from '../layout/Meta';
-import { AppConfig } from '../utils/AppConfig';
-import { Banner } from './Banner';
-import { Footer } from './Footer';
-import { Hero } from './Hero';
-import { Sponsors } from './Sponsors';
-import { VerticalFeatures } from './VerticalFeatures';
+import { Meta } from "../layout/Meta";
+import { AppConfig } from "../utils/AppConfig";
+import { Banner } from "./Banner";
+import { Footer } from "./Footer";
+import { Hero } from "./Hero";
+import { Navbar } from "./Navbar";
+import { Sponsors } from "./Sponsors";
+import { VerticalFeatures } from "./VerticalFeatures";
 
 const Base = () => (
   <div className="text-gray-600 antialiased">
     <Meta title={AppConfig.title} description={AppConfig.description} />
+    <Navbar />
     <Hero />
     {/* <Sponsors /> */}
     <VerticalFeatures />
